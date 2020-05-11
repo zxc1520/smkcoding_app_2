3a2a31db67b8fc998689309a4d722f9325ce3d4c
zxc1520 <afifghulam21@gmail.com>
2020-05-11 13:41:28 +0700

    Rest API - get github user

--- app/src/main/java/com/smkcoding/myapplication/fragment/GithubFragment.kt
+++ app/src/main/java/com/smkcoding/myapplication/fragment/GithubFragment.kt
@@ -1,12 +1,26 @@
 package com.smkcoding.myapplication.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.smkcoding.myapplication.GithubUser
+import com.smkcoding.myapplication.GithubUserAdapter
+import com.smkcoding.myapplication.GithubUserItem
 import com.smkcoding.myapplication.R
+import com.smkcoding.myapplication.data.GithubService
+import com.smkcoding.myapplication.data.apiRequest
+import com.smkcoding.myapplication.data.httpClient
+import com.smkcoding.myapplication.util.dismissLoading
+import com.smkcoding.myapplication.util.showLoading
+import com.smkcoding.myapplication.util.tampilToast
+import kotlinx.android.synthetic.main.frame_github.*
+import retrofit2.*
 
 class GithubFragment : Fragment() {
 
@@ -27,6 +41,52 @@ class GithubFragment : Fragment() {
         @Nullable savedInstanceState: Bundle?
     ) {
         super.onViewCreated(view, savedInstanceState)
+        callApiGetGithubUser()
+    }
+
+    fun callApiGetGithubUser() {
+        showLoading(context!!, swipeRefresLayout)
+
+        val httpClient = httpClient()
+        val apiRequest = apiRequest<GithubService>(httpClient)
+
+        val call = apiRequest.getUsers()
+        call.enqueue(object :
+            Callback<List<GithubUserItem>> {
+            override fun onFailure(call: Call<List<GithubUserItem>>, t: Throwable) {
+                dismissLoading(swipeRefresLayout)
+            }
+
+            override fun onResponse(
+                call: Call<List<GithubUserItem>>,
+                response: Response<List<GithubUserItem>>
+            ) {
+                dismissLoading(swipeRefresLayout)
+
+                when {
+                    response.isSuccessful ->
+                        when {
+                            response.body()?.size != 0 -> tampilGithubUser(response.body()!!)
+                            else -> {
+                                tampilToast(context!!, "Berhasil")
+                            }
+                        }
+                    else -> {
+                        tampilToast(context!!, "Gagal")
+                    }
+                }
+            }
+
+        })
+
+    }
+
+    private fun tampilGithubUser(githubUsers: List<GithubUserItem>) {
+        listGithubUser.layoutManager = LinearLayoutManager(context)
+        listGithubUser.adapter = GithubUserAdapter(context!!, githubUsers) {
+            val githubUser = it
+            tampilToast(context!!, githubUser.login)
+        }
     }
 
 }

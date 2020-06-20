a4614eded718ef6c03126bfde740a323f3f780c2
zxc1520 <afifghulam21@gmail.com>
2020-06-20 21:12:06 +0700

    Login Activity

--- app/src/main/java/com/smkcoding/myapplication/LoginActivity.kt
+++ app/src/main/java/com/smkcoding/myapplication/LoginActivity.kt
@@ -4,59 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var inputName : String = ""
-    private var inputPass : String = ""
+    private var auth: FirebaseAuth? = null
+    private var RC_SIGN_IN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        btnLogin.setOnClickListener { validate() }
-    }
+        progress.visibility = View.GONE
+        login.setOnClickListener(this)
 
-    private fun validate() {
-        inputName = edtNama.text.toString()
-        inputPass = edtPass.text.toString()
+        auth = FirebaseAuth.getInstance()
 
-        when {
-            inputName.isEmpty() -> edtNama.error = "Username tidak boleh kosong"
-            inputPass.isEmpty() -> edtPass.error = "Password tidak boleh kosong"
-            else -> {
-                showToast("Berhasil Login")
-                goToMain()
-            }
+        if (auth!!.currentUser == null) {
+
+        } else {
+             intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 
-    private fun goToMain() {
-        val intent = Intent(this, MainActivity::class.java)
-        val pref = PreferenceManager.getDefaultSharedPreferences(this)
-        pref.apply {
-            val userName = getString("NAME", "")
-            val passWord = getString("PASS", "")
-            edtNama.setText(userName)
-            edtPass.setText(passWord)
-        }
 
-        saveData()
-    }
-
-    private fun saveData() {
-
-        val pref = PreferenceManager.getDefaultSharedPreferences(this)
-        val editor = pref.edit()
-        editor.putString("NAME", edtNama.text.toString()).putString("PASS", edtPass.text.toString())
-        startActivity(Intent(this, MainActivity::class.java))
-
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
 
 }
